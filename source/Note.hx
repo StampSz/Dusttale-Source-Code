4d19940bc32f0e7f0bf5cc9ccffacc19d3393cd9
WassabiSoja <88618149+WassabiSoja@users.noreply.github.com>
2021-08-18 23:56:47 +0200

    notes added

--- source/Note.hx
+++ source/Note.hx
@@ -129,6 +129,29 @@ class Note extends FlxSprite
 								updateHitbox();
 								antialiasing = true;
 							}
+
+							case 3:
+							{
+								frames = Paths.getSparrowAtlas('NotesKR/NOTE_assets_Dust');
+								animation.addByPrefix('greenScroll', 'green0');
+								animation.addByPrefix('redScroll', 'red0');
+								animation.addByPrefix('blueScroll', 'blue0');
+								animation.addByPrefix('purpleScroll', 'purple0');
+					
+								animation.addByPrefix('purpleholdend', 'pruple end hold');
+								animation.addByPrefix('greenholdend', 'green hold end');
+								animation.addByPrefix('redholdend', 'red hold end');
+								animation.addByPrefix('blueholdend', 'blue hold end');
+					
+								animation.addByPrefix('purplehold', 'purple hold piece');
+								animation.addByPrefix('greenhold', 'green hold piece');
+								animation.addByPrefix('redhold', 'red hold piece');
+								animation.addByPrefix('bluehold', 'blue hold piece');
+					
+								setGraphicSize(Std.int(width * 0.7));
+								updateHitbox();
+								antialiasing = true;
+							}
 							default:
 							{
 								frames = Paths.getSparrowAtlas('NOTE_assets');
