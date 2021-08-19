e9db401bd7c4940decb7992580b1d3eafd2cfdf6
StampSz <87995546+StampSz@users.noreply.github.com>
2021-08-19 20:47:57 +0100

    gasterblaster function

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1092,9 +1092,6 @@ class PlayState extends MusicBeatState
 		DustCloud1.visible = true;
 		DustCloud1.setGraphicSize(Std.int(DustCloud1.width * 1));
 		DustCloud1.alpha = 0;
-		
-
-
 
 		// Add Kade Engine watermark
 		kadeEngineWatermark = new FlxText(4,healthBarBG.y + 50,0,SONG.song + " - " + CoolUtil.difficultyFromInt(storyDifficulty) + (Main.watermarks ? "" + MainMenuState.kadeEngineVer : ""), 16);
@@ -3940,6 +3937,23 @@ class PlayState extends MusicBeatState
 	var fastCarCanDrive:Bool = true;
 	var dustAccumulated:Int = 0;
 
+	function gasterBlasters():Void
+	{
+		var blasters:FlxSprite = new FlxSprite(0,0);
+
+		blasters.frames = Paths.getSparrowAtlas('gasterBlaster');
+        blasters.animation.addByPrefix('shoot', 'shoot', 24, false);
+        blasters.setPosition(-1170.1, 346.95);
+
+		add(blasters);
+		 blasters.visible = true;
+
+        blasters.animation.play('shoot');
+	    {
+		health -= 0.4;
+		}
+	}
+
 	function DustCloud():Void
 	{
 		dustAccumulated++;
@@ -4046,23 +4060,6 @@ class PlayState extends MusicBeatState
 				dad.playAnim('idle');
 			});
 	}
-
-	function gasterBlasters():Void
-    {
-        var blasters:FlxSprite = new FlxSprite(0,0);
-
-        blasters.frames = Paths.getSparrowAtlas('gasterBlaster');
-        blasters.animation.addByPrefix('shoot', 'shoot', 24, false);
-        blasters.setPosition(-1170.1, 346.95);
-
-        add(blasters);
-         blasters.visible = true;
-
-        blasters.animation.play('shoot');
-        {
-        health -= 0.4;
-        }
-    }
 
 
 	function flipCamDown():Void
@@ -4240,12 +4237,12 @@ class PlayState extends MusicBeatState
 		//psychotic-breakdown
 
 
+
 		if (curStep == 20 && curSong.toLowerCase() == 'psychotic-breakdown')
-        {
-            gasterBlasters();
+		{
+			gasterBlasters();
 
-        }
-
+		}
 
 		if (curStep == 512 && curSong.toLowerCase() == 'psychotic-breakdown')
 		{
