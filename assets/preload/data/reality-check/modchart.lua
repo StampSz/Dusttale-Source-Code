a9a76a0bd81d27049122154fa530f63c8c9170a5
StampSz <87995546+StampSz@users.noreply.github.com>
2021-08-30 15:05:07 +0100

    mod chart reality check

--- assets/preload/data/reality-check/modchart.lua
+++ assets/preload/data/reality-check/modchart.lua
@@ -3,7 +3,12 @@ function start (song)
 end
 
 function update (elapsed)
-	
+	if curBeat < 2449 then	
+		local currentBeat = (songPos / 1000)*(bpm/150)
+		for i=0,7 do
+		setActorY(defaultStrum0Y + 10 * math.cos((currentBeat + i*5.25) * math.pi), i)
+	end
+end
 end
 
 function beatHit (beat)
